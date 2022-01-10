ebb98813ad9dffdadf68e122c2ba156c34cf3095
Jane Moroz <Moroz.jane@yahoo.com>
2022-01-10 15:19:51 +0300

    Created Module

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,31 +1,11 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const replaceTemplate = require('./modules/replaceTemplate')
+
 /////////////////////////////////////////////////////////////////////////
 // Building Simple API
 /////////////////////////////////////////////////////////////////////////
-
-// To Access File System
-const fs = require("fs");
-
-const http = require("http");
-const url = require("url");
-
-// Replace template
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return output;
-};
 
 // Get templates
 const tempOverview = fs.readFileSync(
